3c10c5c909c410c4c0dce48f4a32b312319b51ad
Haroon Albar <haroonalbarx@gmail.com>
2024-07-03 17:10:30 +0530

    feat: loading env variables

--- main.go
+++ main.go
@@ -4,9 +4,27 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// load from .env
+	godotenv.Load()
+
+	// get port
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT is not in the env")
+	}
+	// get db_url
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL is not in the env")
+	}
+
+
 	mux := http.NewServeMux()
 
 	fmt.Println("Start server on port 4000")
