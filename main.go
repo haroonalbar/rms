dda1959566d7d48da005c7c34aab0bbfabd556fb
Haroon Albar <haroonalbarx@gmail.com>
2024-07-03 16:43:22 +0530

    chore: remove example env will add the instructions on readme

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 )
 
 func main() {
-	fmt.Println("RMS is here")
 	mux := http.NewServeMux()
+
 	fmt.Println("Start server on port 4000")
 	err := http.ListenAndServe(":4000", mux)
 	if err != nil {
