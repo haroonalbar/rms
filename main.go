b2965fcb59cef062b8c468031600b8ea1e8e04d3
Haroon Albar <haroonalbarx@gmail.com>
2024-07-03 16:40:54 +0530

    feat: Create and listen to the server

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("RMS is here")
+	mux := http.NewServeMux()
+	fmt.Println("Start server on port 4000")
+	err := http.ListenAndServe(":4000", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
